d347779076f8070c28bd92d792b822c61dac6776
Garu <lolpmmo@gmail.com>
2024-01-06 23:07:04 +0100

    Calls a Message Box function that take multiple arguments

--- message_box/src/main.rs
+++ message_box/src/main.rs
@@ -1,6 +1,6 @@
 use winapi::um::winuser::MessageBoxW;
 
-fn message_box(content: &mut String, title: &mut String, button: u32, icon: u32) -> i32 {
+fn message_box(content: &str, title: &str, button: u32, icon: u32) -> i32 {
     unsafe {
         let last_result = MessageBoxW(
             std::ptr::null_mut(),
@@ -13,18 +13,12 @@ fn message_box(content: &mut String, title: &mut String, button: u32, icon: u32)
 }
 
 fn main() {
-    let mut content = "Do you want to continue ?\0".to_string();
-    let mut title = "Test 1/2\0".to_string();
-    let last_result = message_box(&mut content, &mut title, 0x00000004, 0x00000030);
+    let last_result = message_box("Do you want to continue ?\0", "Test 1/2\0", 0x00000004, 0x00000030);
 
     if last_result == 6 {
-        content = "You continued the test\0".to_string();
-        title = "Test 2/2\0".to_string();
-        message_box(&mut content, &mut title, 0x000000000, 0x000000040);
+        message_box("You continued the test\0", "Test 2/2\0", 0x000000000, 0x000000040);
     } else {
-        content = "Goodbye\0".to_string();
-        title = "Exit\0".to_string();
-        message_box(&mut content, &mut title, 0x00000003, 0x000000020);
+        message_box("Goodbye\0", "Exit\0", 0x00000003, 0x000000020);
     }
 }
 
