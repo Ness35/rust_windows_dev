35d456fda35278fed624af0a404e766b4e04c22d
Garu <lolpmmo@gmail.com>
2024-01-06 23:02:47 +0100

    Calls a Message Box function that take multiple arguments

--- message_box/src/main.rs
+++ message_box/src/main.rs
@@ -1,12 +1,12 @@
-use winapi::um::winuser::{MessageBoxW, MB_ICONEXCLAMATION, MB_YESNO};
+use winapi::um::winuser::MessageBoxW;
 
-fn message_box(content: &mut String, title: &mut String) -> i32 {
+fn message_box(content: &mut String, title: &mut String, button: u32, icon: u32) -> i32 {
     unsafe {
         let last_result = MessageBoxW(
             std::ptr::null_mut(),
             content.encode_utf16().collect::<Vec<_>>().as_mut_ptr(),
             title.encode_utf16().collect::<Vec<_>>().as_mut_ptr(),
-            MB_ICONEXCLAMATION | MB_YESNO,
+            icon | button,
         );
         last_result
     }
@@ -15,16 +15,16 @@ fn message_box(content: &mut String, title: &mut String) -> i32 {
 fn main() {
     let mut content = "Do you want to continue ?\0".to_string();
     let mut title = "Test 1/2\0".to_string();
-    let last_result = message_box(&mut content, &mut title);
+    let last_result = message_box(&mut content, &mut title, 0x00000004, 0x00000030);
 
     if last_result == 6 {
         content = "You continued the test\0".to_string();
         title = "Test 2/2\0".to_string();
-        message_box(&mut content, &mut title);
+        message_box(&mut content, &mut title, 0x000000000, 0x000000040);
     } else {
         content = "Goodbye\0".to_string();
         title = "Exit\0".to_string();
-        message_box(&mut content, &mut title);
+        message_box(&mut content, &mut title, 0x00000003, 0x000000020);
     }
 }
 
