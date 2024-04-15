793bf6066ef74ff897ec7d7ef1f5323896e207ee
Nes35 <lolpmmo@gmail.com>
2024-04-15 22:10:29 +0200

    Message box function that take multiple arguments

--- message_box/src/main.rs
+++ message_box/src/main.rs
@@ -1,5 +1,6 @@
 use winapi::um::winuser::MessageBoxW;
 
+/*-----Message box function-----*/
 fn message_box(content: &str, title: &str, button: u32, icon: u32) -> i32 {
     unsafe {
         let last_result = MessageBoxW(
@@ -13,12 +14,12 @@ fn message_box(content: &str, title: &str, button: u32, icon: u32) -> i32 {
 }
 
 fn main() {
-    let last_result = message_box("Do you want to continue ?\0", "Test 1/2\0", 0x00000004, 0x00000030);
+    let last_result = message_box("Do you want to continue ?\0", "Test 1/2\0", 0x4, 0x30);
 
     if last_result == 6 {
-        message_box("You continued the test\0", "Test 2/2\0", 0x000000000, 0x000000040);
+        message_box("You continued the test\0", "Test 2/2\0", 0x000000000, 0x40);
     } else {
-        message_box("Goodbye\0", "Exit\0", 0x00000003, 0x000000020);
+        message_box("Goodbye\0", "Exit\0", 0x3, 0x20);
     }
 }
 
